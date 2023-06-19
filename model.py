0b39855610e26343fcbfaf70b51c3e187956f977
Jingming <jingmingzhuo@gmail.com>
2023-06-19 08:35:10 +0800

    fix: change function get_response

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ def completions_with_backoff(**kwargs):
 
 def get_response(prompt, model="gpt-3.5-turbo", temperature=0, max_tokens=300, n=1, stop=None) -> list:
     messages = [{"role": "user", "content": prompt}]
-    out = completions_with_backoff(model=model, messages=messages, temperature=temperature, max_tokens=max_tokens, n=1, stop=stop)
+    out = completions_with_backoff(model=model, messages=messages, temperature=temperature, max_tokens=max_tokens, n=n, stop=stop)
     response = {}
     response["content"] = [choice["message"]["content"] for choice in out["choices"]]
     # response["content"] = [out["choices"][0]["message"]["content"]]
